84a718b59c2388cef5fe4c37ebd9cf3ab6c60002
abhaykamath <abhaykamath2299@gmail.com>
2025-08-06 21:22:03 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-[100dvh] flex justify-center items-center bg-background">
+      <h1 className="text-5xl">Hey, Anush !😁</h1>
+    </div>
+  );
+}
+
+export default App;
